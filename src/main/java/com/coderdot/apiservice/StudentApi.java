386cfc20eb89301127cd3bd1da996a38f31087e5
subodh kumar <subodh.sawan@gmail.com>
2025-01-29 21:11:44 +0530

    crud url added

--- src/main/java/com/coderdot/apiservice/StudentApi.java
+++ src/main/java/com/coderdot/apiservice/StudentApi.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,8 +16,11 @@ public class StudentApi {
 	@Autowired
 	private RestTemplate resttemplate;
 	
+	@Value("${student-crud-api}")
+	private String baseurl;
+	
 	public List<Student> listStudent(){
-		 String url = "http://localhost:8181/student/getallStudent"; // Example API
+		 String url = baseurl+"/student/getallStudent"; // Example API
 	         List<Student> forObject = resttemplate.getForObject(url, ArrayList.class);
 	         return forObject;
 	}
